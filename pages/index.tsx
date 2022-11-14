c361c2e11d04a4d67687a4b98aeb28e88cdeab8f
hehehai <riverhohai@gmail.com>
2022-11-14 16:57:16 +0800

    feat: all publised posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,46 @@
+import React from "react";
+import { GetStaticProps } from "next";
 import Layout from "~/components/Layout";
+import Post, { PostProps } from "~/components/Post";
+import { db } from "~/lib/db/prisma";
 
-export default function () {
+export const getStaticProps: GetStaticProps = async () => {
+  const feed = await db.post.findMany({
+    where: {
+      published: true,
+    },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+    // 降序，最新创建的文章在最前
+    orderBy: { createdAt: "desc" },
+  });
+  return {
+    props: { feed },
+    // 10 秒
+    revalidate: 10,
+  };
+};
+
+type Props = {
+  feed: PostProps[];
+};
+
+const Home: React.FC<Props> = (props) => {
   return (
     <Layout>
-      <main className="bg-white">
-        <div className="text-red-500 p-4">hello</div>
+      <h1 className="mb-3">最新文章</h1>
+      <main className="space-y-6">
+        {props.feed.map((post) => (
+          <div key={post.id}>
+            <Post post={post} />
+          </div>
+        ))}
       </main>
     </Layout>
   );
-}
+};
+
+export default Home;
